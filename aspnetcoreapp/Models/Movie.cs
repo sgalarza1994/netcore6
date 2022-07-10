dc9e5b0ccaa8b2b9cc34b284a09a163fe03ecbe7
steven galarza <stegalarza@hotmail.com>
2022-07-09 22:19:07 -0500

    change initial

---
+++ aspnetcoreapp/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace aspnetcoreapp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } =string.Empty;
+
+
+        [DataType(DataType.Date)]
+        public DateTime RealeseDate { get; set; }
+        public string GenRe { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }  
+
+
+    }
+}
---
+++ aspnetcoreapp/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using aspnetcoreapp.Data;
+using aspnetcoreapp.Models;
+
+namespace aspnetcoreapp.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly aspnetcoreapp.Data.aspnetcoreappContext _context;
+
+        public IndexModel(aspnetcoreapp.Data.aspnetcoreappContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Movie != null)
+            {
+                Movie = await _context.Movie.ToListAsync();
+            }
+        }
+    }
+}
